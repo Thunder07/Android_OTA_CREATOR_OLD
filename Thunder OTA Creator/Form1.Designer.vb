40a85872296435cf2e2e9e2c1b3ab488fb3058e3
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-01-08 22:16:17 +0000

    Initial Commit

---
+++ Thunder OTA Creator/Form1.Designer.vb
@@ -0,0 +1,305 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(9, 32)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(255, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(9, 72)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(255, 21)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Old ROM:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Newer ROM:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(9, 138)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Refresh"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(178, 138)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(86, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Process"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(258, 149)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ProgressBar1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(273, 175)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Options"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(90, 138)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(82, 23)
+        Me.ProgressBar1.TabIndex = 11
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(9, 112)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(255, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "OTA Folder Name:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 193)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(273, 203)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Logger"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(3, 174)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(261, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 32)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(252, 90)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 16)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(28, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Files"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.TextBox4)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Location = New System.Drawing.Point(291, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(273, 240)
+        Me.GroupBox3.TabIndex = 10
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Exceptions"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 125)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Folders"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(12, 141)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(252, 90)
+        Me.TextBox4.TabIndex = 9
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label6)
+        Me.GroupBox4.Controls.Add(Me.PictureBox1)
+        Me.GroupBox4.Location = New System.Drawing.Point(291, 258)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(273, 138)
+        Me.GroupBox4.TabIndex = 11
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "About/Donations"
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(11, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(253, 63)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Simple App I (Thunder07) Made For XDA Members To Easily Compare Files And Pick Th" & _
+    "e Differences Between 2 ROM Update, Thus Creating An OTA Package"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Thunder_OTA_Creator.My.Resources.Resources.btn_donateCC_LG
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 82)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(261, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 408)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(312, 446)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Thunder07 OTA Creator V1.2"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ Thunder OTA Creator/Form1.vb
@@ -0,0 +1,295 @@
+﻿Imports System.Security.Cryptography
+Imports System.Threading
+Public Class Form1
+    Private Shared P1 As String
+    Private Shared P2 As String
+    Private Shared NewN As String
+    Private Shared DNO As Integer = 0
+    Private Shared DON As Integer = 0
+    Private Shared T1 As Thread
+    Private Shared T2 As Thread
+    Private Shared ok As Date
+
+    
+    Private Sub Process(sender As Object, e As EventArgs) Handles Button2.Click
+        Try : T1.Abort() : Catch ex As Exception : End Try
+        Try : T2.Abort() : Catch ex As Exception : End Try
+        ok = Date.Now
+        TextBox2.Text = TextBox2.Text.Trim
+        If ComboBox1.SelectedIndex = -1 Or ComboBox2.SelectedIndex = -1 Or TextBox2.Text = "" Then
+            MsgBox("Please Choose Fill-In The Options Before Proceeding")
+            Exit Sub
+        End If
+        TextBox2.Enabled = False
+        ComboBox1.Enabled = False : ComboBox2.Enabled = False
+        Button1.Enabled = False : Button2.Enabled = False : Button3.Enabled = False
+        TextBox3.Enabled = False : TextBox4.Enabled = False
+        TextBox3.Text = TextBox3.Text.Trim : TextBox4.Text = TextBox4.Text.Trim
+        P1 = ComboBox1.Text
+        P2 = ComboBox2.Text
+        NewN = TextBox2.Text
+        Dim t As New Thread(AddressOf Compare)
+        t.Start()
+        ProgressBar1.Style = ProgressBarStyle.Marquee
+        ProgressBar1.Value = 1
+    End Sub
+
+    Private Sub Compare()
+       Dim ss As String = Application.StartupPath & "\ROM\"
+        Dim path1 As String = ss & P1
+        Dim path2 As String = ss & P2
+        T1 = New Thread(AddressOf DNOD)
+        T2 = New Thread(AddressOf DOND)
+        t1.Name = "T1"
+        t2.Name = "T2"
+
+        t1.Start(New IO.DirectoryInfo(path1))
+        t2.Start(New IO.DirectoryInfo(path2))
+        ' folderDON(New IO.DirectoryInfo(path1))
+        'folderDNO(New IO.DirectoryInfo(path2))
+        'If t1.IsAlive Then
+        '    t1.Join()
+        'End If
+        'If t2.IsAlive Then
+        '    t2.Join()
+        'End If
+
+
+    End Sub
+
+    Private Function compareFileMd5(ByVal filename As String, ByVal oldp As String, ByVal newp As String) As Boolean
+
+        ' get all the file contents
+        Dim File1() As Byte = System.IO.File.ReadAllBytes(oldp & "\" & filename)
+        ' create a new md5 object
+        Dim Md5 As New MD5CryptoServiceProvider()
+        ' compute the hash
+        Dim byteHash1() As Byte = Md5.ComputeHash(File1)
+        ' return the value in base 64
+
+        Dim File2() As Byte = System.IO.File.ReadAllBytes(newp & "\" & filename)
+        ' compute the hash
+        Dim byteHash2() As Byte = Md5.ComputeHash(File2)
+        ' return the value in base 64
+        If Convert.ToBase64String(byteHash2) = Convert.ToBase64String(byteHash1) Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Function compareOLD2NEW(ByVal filename As String, ByVal oldp As String, ByVal newp As String) As Boolean
+        If My.Computer.FileSystem.FileExists(oldp & "\" & filename) Then
+            If My.Computer.FileSystem.FileExists(newp & "\" & filename) Then
+                If compareFileMd5(filename, oldp, newp) Then
+                    Return True
+                End If
+            Else
+                Return False
+            End If
+        End If
+        Return False
+    End Function
+    Private Function compareNEW2OLD(ByVal filename As String, ByVal oldp As String, ByVal newp As String) As Boolean
+        If My.Computer.FileSystem.FileExists(newp & "\" & filename) Then
+            If My.Computer.FileSystem.FileExists(oldp & "\" & filename) Then
+                If compareFileMd5(filename, oldp, newp) Then
+                    Return True
+                End If
+            Else
+                Return True
+            End If
+        End If
+        Return False
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ComboBox1.Items.Clear()
+        ComboBox2.Items.Clear()
+        Try
+            Dim di As New IO.DirectoryInfo(Application.StartupPath + "\ROM\")
+            Dim diar1() As IO.DirectoryInfo = di.GetDirectories()
+            For x = 0 To diar1.Length() - 1
+                ComboBox1.Items.Add(diar1(x).Name())
+                ComboBox2.Items.Add(diar1(x).Name())
+            Next
+        Catch ex As Exception
+        End Try
+    End Sub
+
+
+    
+
+    Private Sub folderDNO(ByVal Folder As IO.DirectoryInfo)
+        For Each Folder2 As IO.DirectoryInfo In Folder.GetDirectories
+            For Each File As IO.FileInfo In Folder2.GetFiles
+                If compareNEW2OLD(File.Name, Folder2.FullName, Folder2.FullName.Replace(P1, P2)) Then
+                    DNO = 0
+AnotherGo:          Try
+                        My.Computer.FileSystem.CopyFile(File.FullName.ToString.Replace(P1, P2), File.FullName.ToString.Replace(P1, NewN).Replace("ROM", "OTA"), True)
+                    Catch ex As Exception
+                        DNO += 1
+                        If DNO < 5 Then
+                            Thread.Sleep(5000)
+                            GoTo AnotherGo
+                        End If
+                        '''''Report the issue and skip
+                        Dim del As New TBD(AddressOf TB)
+                        del.Invoke("Failed To Copy " & File.FullName.Replace(Application.StartupPath & "\ROM\", "") & vbNewLine)
+                    End Try
+                End If
+            Next
+            folderDNO(Folder2)
+        Next
+    End Sub
+    Private Sub folderDON(ByVal Folder As IO.DirectoryInfo)
+        For Each Folder2 As IO.DirectoryInfo In Folder.GetDirectories
+            For Each File As IO.FileInfo In Folder2.GetFiles
+                If compareOLD2NEW(File.Name, Folder2.FullName, Folder2.FullName.Replace(P1, P2)) Then
+                    DON = 0
+AnotherGo:          Try
+                        My.Computer.FileSystem.CopyFile(File.FullName, File.FullName.ToString.Replace(P2, NewN).Replace("ROM", "OTA"), True)
+                    Catch ex As Exception
+                        DON += 1
+                        If DON < 5 Then
+                            Thread.Sleep(5000)
+                            GoTo AnotherGo
+                        End If
+                        '''''Report the issue and skip
+                        Dim del As New TBD(AddressOf TB)
+                        del.Invoke("Failed To Copy " & File.FullName.Replace(Application.StartupPath & "\ROM\", "") & vbNewLine)
+                    End Try
+                End If
+            Next
+            folderDON(Folder2)
+        Next
+    End Sub
+    Private Sub DNOD(ByVal Folder As IO.DirectoryInfo)
+        folderDNO(Folder)
+        If T2.IsAlive = False Then
+            Dim del As New FinishD(AddressOf Finish)
+            Me.Invoke(del)
+        End If
+    End Sub
+    Private Sub DOND(ByVal Folder As IO.DirectoryInfo)
+        folderDON(Folder)
+        If T1.IsAlive = False Then
+            Dim del As New FinishD(AddressOf Finish)
+            Me.Invoke(del)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try : My.Computer.FileSystem.CreateDirectory(Application.StartupPath & "\ROM\") : Catch ex As Exception : End Try
+        Try : My.Computer.FileSystem.CreateDirectory(Application.StartupPath & "\OTA\") : Catch ex As Exception : End Try
+    End Sub
+    Private Sub Delay(ByVal dblSecs As Double)
+        Const OneSec As Double = 1.0# \ (1440.0# * 60.0#)
+        Dim dblWaitTil As Date
+        Now.AddSeconds(OneSec)
+        dblWaitTil = Now.AddSeconds(OneSec).AddSeconds(dblSecs)
+        Do Until Now > dblWaitTil
+            Application.DoEvents() ' Allow windows messages to be processed
+        Loop
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Clear()
+    End Sub
+
+   
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        TextBox2.Text = TextBox2.Text.Replace("\", "").Replace("/", "").Replace("*", "").Replace(":", "").Replace("?", "").Replace("<", "").Replace(">", "").Replace("""", "").Replace("|", "")
+        '\ / * : ? " <> |
+        TextBox2.Select(TextBox2.Text.Length, 0)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            TextBox1.SelectionStart = TextBox1.Text.Length - 1
+            TextBox1.SelectionLength = 0
+            TextBox1.ScrollToCaret()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Delegate Sub TBD(ByRef text As String)
+    Private Sub TB(ByRef text As String)
+        TextBox1.Text &= text
+    End Sub
+    Private Delegate Sub FinishD()
+    Private Sub Finish()
+        Excpetion()
+        Dim ok2 As TimeSpan = Date.Now.Subtract(ok)
+        TextBox1.Text &= "Process Took " & ok2.Minutes & " Minutes And " & ok2.Seconds & " Seconds" & vbNewLine
+        TextBox2.Enabled = True
+        ComboBox1.Enabled = True : ComboBox2.Enabled = True
+        Button1.Enabled = True : Button2.Enabled = True : Button3.Enabled = True
+        TextBox3.Enabled = True : TextBox4.Enabled = True
+        P1 = Nothing
+        P2 = Nothing
+        NewN = Nothing
+        ProgressBar1.Value = 100
+        ProgressBar1.Style = ProgressBarStyle.Blocks
+    End Sub
+
+    Private Sub Excpetion()
+        TextBox3.Text = TextBox3.Text.Trim
+        TextBox4.Text = TextBox4.Text.Trim
+        Dim path As String = Application.StartupPath & "\ROM\" & ComboBox2.Text & "\"
+        If TextBox3.Text.Length > 0 Then
+            Dim tmpfile() As String
+            Try
+                tmpfile = TextBox3.Text.Split(vbNewLine)
+            Catch ex As Exception
+                tmpfile = {TextBox3.Text}
+            End Try
+            For Each file In tmpfile
+                file = file.Trim
+                DON = 0
+AnotherGo:      Try
+                    My.Computer.FileSystem.CopyFile(path & file, path.ToString.Replace(P2, NewN).Replace("ROM", "OTA") & file, True)
+                Catch ex As Exception
+                    DON += 1
+                    If DON < 5 Then
+                        Thread.Sleep(5000)
+                        GoTo AnotherGo
+                    End If
+                    '''''Report the issue and skip
+                    Dim del As New TBD(AddressOf TB)
+                    del.Invoke("Failed To Copy " & file & vbNewLine)
+                End Try
+            Next
+        End If
+        If TextBox4.Text.Length > 0 Then
+            Dim tmpdir() As String
+            Try
+                tmpdir = TextBox4.Text.Split(vbNewLine)
+            Catch ex As Exception
+                tmpdir = {TextBox4.Text}
+            End Try
+            For Each direc In tmpdir
+                direc = direc.Trim
+                DON = 0
+AnotherGo2:     Try
+                    My.Computer.FileSystem.CopyDirectory(path & direc, path.ToString.Replace(P2, NewN).Replace("ROM", "OTA") & direc, True)
+                Catch ex As Exception
+                    DON += 1
+                    If DON < 5 Then
+                        Thread.Sleep(5000)
+                        GoTo AnotherGo2
+                    End If
+                    '''''Report the issue and skip
+                    Dim del As New TBD(AddressOf TB)
+                    del.Invoke("Failed To Copy " & direc & vbNewLine)
+                End Try
+            Next
+        End If
+
+    End Sub
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Diagnostics.Process.Start("http://forum.xda-developers.com/donatetome.php?u=618483")
+    End Sub
+End Class
